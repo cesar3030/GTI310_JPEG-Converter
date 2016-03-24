3b9100703d5479ef369006f8b69ccb08f8649469
César Jeanroy <cesar.jeanroy@gmail.com>
2016-03-24 19:43:30 -0400

    Adding the ZigZag class

---
+++ test/gti310/tp4/ZigZagTest.java
@@ -0,0 +1,16 @@
+package gti310.tp4;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by César Jeanroy on 2016-03-24.
+ */
+public class ZigZagTest {
+
+    @Test
+    public void testConvertToZigzag() throws Exception {
+
+    }
+}
