cb22c277637008d27b1e9de4fad102d9793e7c2c
César Jeanroy <cesar.jeanroy@gmail.com>
2016-04-06 23:07:28 -0400

    Adding the AC and DC conversion and moving the zigzag test class in the good folder

---
+++ gti310/tp4/logic/AcDcConversion.java
@@ -0,0 +1,17 @@
+package gti310.tp4.logic;
+
+import gti310.tp4.model.ImageData;
+
+/**
+ * Created by César Jeanroy on 2016-04-06.
+ * Class that calculate the RLC and the DPCM on image's matrices
+ */
+public class AcDcConversion {
+
+    public static void process(ImageData image){
+        for (int[][] matrix :image.getYMatrices()) {
+
+        }
+    }
+
+}
---
+++ test/gti310/tp4/logic/AcDcConversionTest.java
@@ -0,0 +1,16 @@
+package gti310.tp4.logic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by César Jeanroy on 2016-04-06.
+ */
+public class AcDcConversionTest {
+
+    @Test
+    public void testProcess() throws Exception {
+
+    }
+}
