de63189eebdd0a56eb631c95455008ca2f6047ee
César Jeanroy <cesar.jeanroy@gmail.com>
2016-04-08 18:23:13 -0400

    The whole process is now working. DCTI issue has been fixed, YCbCr conversion has been fixed, Quantification alfa calcul has been fixed.

---
+++ test/gti310/tp4/logic/RGBtoYCbCrTest.java
@@ -0,0 +1,21 @@
+package gti310.tp4.logic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by César Jeanroy on 2016-04-08.
+ */
+public class RGBtoYCbCrTest {
+
+    @Test
+    public void testConversionRGBtoYCbCr() throws Exception {
+
+    }
+
+    @Test
+    public void testConversionYCbCrtoRGB() throws Exception {
+
+    }
+}
